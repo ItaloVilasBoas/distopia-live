10c8a2ea3a09c3a5e2906f29bc12c5dc1a66f2dd
Thiago-Mota-Santos <lucimara1422@gmail.com>
2024-10-23 19:42:54 -0300

    feat(spacing) improve

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,16 +5,15 @@ export default function Home() {
   return (
     <header className="flex items-center justify-between px-6 py-4 bg-white border-b">
       <div className="text-2xl font-bold">distopia.</div>
-      <nav className="absolute left-1/2 transform -translate-x-1/2 flex items-center space-x-6">
-       {/* TODO: add link here in the future */}
+      <nav className="absolute left-1/2 transform -translate-x-1/2 flex items-center space-x-12">
         <a href="#" className="text-gray-600 hover:text-black">
           Membros
         </a>
-        <div className="h-6 w-px bg-gray-300"></div>
+        <div className="h-6 w-[3px] bg-gray-600"></div>
         <a href="#" className="bg-black text-white px-4 py-2">
           Home
         </a>
-        <div className="h-6 w-px bg-gray-300"></div>
+        <div className="h-6 w-[3px] bg-gray-600"></div>
         <a href="#" className="text-gray-600 hover:text-black">
           Artes
         </a>
