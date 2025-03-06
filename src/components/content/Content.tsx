663d5845ce7d64c4195fa07f822b82bea6ecd2bf
Leohgb <leonardokgb12@gmail.com>
2025-03-06 19:53:20 -0300

    fix: solving conflicts

--- src/components/content/Content.tsx
+++ src/components/content/Content.tsx
@@ -41,7 +41,6 @@ export default function Content() {
         />
       </div>
 
-<<<<<<< HEAD
       <div className="flex flex-col md:flex-row justify-center items-center w-full md:w-auto px-4 gap-6 md:gap-6">
         <div className="flex gap-4">
           <div className="bg-[#feac2e] w-32 h-10"></div>
@@ -59,23 +58,6 @@ export default function Content() {
           <div className="bg-[#9e4688] w-32 h-10"></div>
           <div className="bg-[#feac2e] w-16 h-10"></div>
           <div className="bg-white w-11 h-10"></div>
-=======
-      <div className="flex justify-between justify-self-center items-center gap-6">
-        <div className="flex gap-4 ssm:gap-1">
-          <div className="bg-[#feac2e] md:w-32 sm:w-16 ssm:w-8 h-10"></div>
-          <div className="bg-[#9e4688] md:w-16 sm:w-8 ssm:w-4 h-10"></div>
-          <div className="bg-white md:w-11 sm:5 ssm:w-2 h-10"></div>
-        </div>
-        <div className="relative">
-          <span className="text-5xl ssm:xl font-extrabold text-white dystopian">distopia.</span>
-          <span className="text-5xl ssm:xl font-extrabold text-white absolute top-6 right-0 dystopian">distopia.</span>
-          <span className="text-5xl font-extrabold text-white absolute bottom-6 right-0 dystopian">distopia.</span>
-        </div>
-        <div className="flex gap-4 ssm:gap-1">
-          <div className="bg-[#9e4688] md:w-32 sm:w-16 ssm:w-8 h-10"></div>
-          <div className="bg-[#feac2e] md:w-16 sm:w-8 ssm:w-4 h-10"></div>
-          <div className="bg-white md:w-11 sm:w-5 ssm:w-2 h-10"></div>
->>>>>>> 83ec84c8009445a5d2224d57d70b67d5ddf38b4d
         </div>
       </div>
 
